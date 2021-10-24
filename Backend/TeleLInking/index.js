a0e561df87068acdef364fd0663b8cb1bc49f13f
piriyaraj <piriyaraj1998@egmail.com>
2021-10-25 01:46:03 +0530

    port change

--- Backend/TeleLInking/index.js
+++ Backend/TeleLInking/index.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const app=express();
+let port = process.env.PORT || 3000;
 
 app.get("/",(req,res)=>{
     res.send("hello telegram backend home")
 })
 
-app.listen(4000)
+app.listen(port, () => {
+    console.log(`App lisition on the localhost port runing on http://localhost:${port}`)
+})
