c0dbad0db609a0dca4773fea12b62a0ada8aa2b9
piriyaraj <piriyaraj1998@egmail.com>
2021-12-27 00:16:57 +0530

    update

--- Backend/CommonTools/index.py
+++ Backend/CommonTools/index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, send_from_directory
 import os
 from threading import Thread
 from AllBikeSpecification import Allbikespecification, ExtractPostLinks, PostMaker
-from Facebook import ActressGallery, DailyQuote, PJTamilLyrics, TamilCineWorld,FightBoysVsGirls,DailyRasi,TamilMemesWorld
+from Facebook import ActressGallery, DailyQuote, PJTamilLyrics, TamilCineWorld, FightBoysVsGirls, DailyRasi, TamilMemesWorld
 app = Flask(__name__)
 
 
