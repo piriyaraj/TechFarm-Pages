b7da2c93eaf3a29455d49d550d95aa4c02b19fa7
piriyaraj <piriyaraj1998@egmail.com>
2022-01-03 16:01:28 +0530

    update

--- Backend/CommonTools/Self/IMGToPDF.py
+++ Backend/CommonTools/Self/IMGToPDF.py
@@ -4,12 +4,12 @@ from PIL import Image
 def converter(images):
     imagelist = []
     if(len(images) == 1):
-        Image.open(images[0]).convert('RGB').save('Self/Data/PJImageConveter.pdf')
+        Image.open(images[0]).convert('RGB').save('app/Self/Data/PJImageConveter.pdf')
         return
     for img in images[1:]:
         imagelist.append(Image.open(img).convert('RGB'))
 
-    Image.open(images[0]).convert('RGB').save('Self/Data/PJImageConveter.pdf', save_all=True,append_images=imagelist)
+    Image.open(images[0]).convert('RGB').save('app/Self/Data/PJImageConveter.pdf', save_all=True,append_images=imagelist)
 
 
 ALLOWED_EXTENSION = set(['png', 'jpg', 'jpeg', 'gif'])
